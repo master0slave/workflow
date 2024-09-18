432d2bb01eabf2642adb294cba67d28aca24d8ef
Nonthiwat <nonthiwat.sct@gmail.com>
2024-09-18 22:52:32 +0700

    add fe

--- workflow-fe/src/app/page.tsx
+++ workflow-fe/src/app/page.tsx
@@ -1,101 +1,107 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { ChangeEvent, FormEvent, useState } from "react";
+import BudgetPanel from "@/components/BudgetPanel";
+import BudgetRequestDataTable from "../components/BudgetRequestDataTable";
+import Header from "@/components/Header";
+import { BudgetRequest } from "@/models/budget-request";
+import FormAddRequest from "@/components/FormAddRequest";
+import DoubleEffect from "@/components/DoubleEffect";
+import CallAPI from "@/components/CallAPI";
+import DemoUseEffect from "@/components/DemoUseEffect";
+import Comp1 from "@/components/DemoContext";
+
+let nextId = 3;
+function Home() {
+  const [budgetRequests, setBudgetRequests] = useState<BudgetRequest[]>([
+    {
+      id: 1,
+      title: "Monitor",
+      amount: 100,
+      quantity: 1,
+      status: "PENDING",
+    },
+    {
+      id: 2,
+      title: "Ram",
+      amount: 200,
+      quantity: 1,
+      status: "APPROVED",
+    },
+    // {
+    //   id: 3,
+    //   title: "CPU",
+    //   amount: 300,
+    //   quantity: 1,
+    //   status: "APPROVED",
+    // },
+  ]);
+  const addRequest = (newRequest: BudgetRequest) => {
+    setBudgetRequests([...budgetRequests, newRequest]);
+  };
+  const [newRequest, setNewRequest] = useState<BudgetRequest>({
+    id: 0,
+    title: "",
+    amount: 0,
+    quantity: 1,
+    status: "APPROVED",
+  });
+
+  const updateField = (event: ChangeEvent<HTMLInputElement>) => {
+    const value =
+      event.target.type === "number"
+        ? Number(event.target.value)
+        : event.target.value;
+    setNewRequest({
+      ...newRequest,
+      [event.target.name]: value,
+    });
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    addRequest({
+      id: nextId++,
+      title: newRequest.title,
+      amount: newRequest.amount,
+      quantity: 1,
+      status: "APPROVED",
+    });
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div>
+      <Header />
+      <main className="container mx-auto">
+        <div className="mt-4">
+          <BudgetPanel items={budgetRequests} />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            Title:
+            <input
+              name="title"
+              value={newRequest.title}
+              onChange={updateField}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div>
+            Amount:
+            <input
+              name="amount"
+              type="number"
+              value={newRequest.amount}
+              onChange={updateField}
+            />
+          </div>
+          <button>Add</button>
+        </form>
+        <div className="mt-4">
+          <BudgetRequestDataTable items={budgetRequests} />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
+
+export default Home;
